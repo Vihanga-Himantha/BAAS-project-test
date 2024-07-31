0c16603dad5e974ee54dda42617061de113395aa
Vihanga-Himantha <vihanga.himantha@gmail.com>
2024-07-31 21:58:54 +0530

    0.2v

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,7 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+<h1>HOME</h1>
 </template>
 
 <script setup>
-defineOptions({
-  name: 'IndexPage'
-});
+
 </script>
