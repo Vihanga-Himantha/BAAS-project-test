06f0e86adaebaa38e2957520bdbf224ad15bd01e
Vihanga-Himantha <vihanga.himantha@gmail.com>
2024-08-01 07:58:49 +0530

    0.4v

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -7,14 +7,15 @@ import { getDatabase } from "firebase/database"; // Import Realtime Database
 import { LocalStorage } from 'quasar'; // Import LocalStorage
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyCY4e3vQGbk-LEPwzNuqSqDMcLhCDyCoPM",
+  authDomain: "baas-project-test.firebaseapp.com",
+  projectId: "baas-project-test",
+  storageBucket: "baas-project-test.appspot.com",
+  messagingSenderId: "416360362388",
+  appId: "1:416360362388:web:52f3c8ba6905a56cba5f81",
+  measurementId: "G-23DRRM62JZ"
 };
 
 // Initialize Firebase
